beaa4eb958f323cbe57d944894814ded79ca310d
Alexander Brittain <alexbrittain9@gmail.com>
2022-11-27 17:03:29 -0600

    Delete FirstInternalHack directory

--- FirstInternalHack/src/scan.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "pch.h"
-
-char* ScanBasic(char* pattern, char* mask, char* begin, intptr_t size);
-char* ScanInternal(char* pattern, char* mask, char* begin, intptr_t size);
